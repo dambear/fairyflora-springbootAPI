0fb9e22d4abb6f89be79d37499116bf644d21403
dambear <denverasi01@gmail.com>
2024-10-04 17:56:02 +0800

    Check Branch, Employee, Inventory, Addon, working all api paths

--- src/main/java/com/danbear/fairyflora/transaction/Transaction.java
+++ src/main/java/com/danbear/fairyflora/transaction/Transaction.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Table(name = "transaction")
 public class Transaction {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @ManyToMany
